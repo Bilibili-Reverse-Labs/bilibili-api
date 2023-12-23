cbe1b56db75ac88aa1cf11b8b23dcb33fdebea3b
qiuxiaori <qiuxiaori@outlook.com>
2023-12-23 19:11:00 +0800

    feat: add test file

---
+++ test/index.js
@@ -0,0 +1,12 @@
+const { expect } = require('chai')
+const fs = require('fs')
+const BilibiliApi = require('../lib/index')
+const api = new BilibiliApi()
+
+describe('登陆验证', function () {
+    it('扫码登陆成功', async function () {
+        const res = await api.login('test/cookie')
+        expect(res).to.be.equal(true)
+        await fs.unlinkSync('test/cookie')
+    })
+})
